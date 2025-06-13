c2a20e5bad809063c3c2ca70a2dda4d57dd7948b
tian <1163586133@qq.com>
2025-06-13 20:49:07 +0800

    【渡一】 1. 组件-优化

--- 渡一/vue入门到实战/代码/04-工程化/src/APP.js
+++ 渡一/vue入门到实战/代码/04-工程化/src/APP.js
@@ -2,10 +2,10 @@ import MyButton from "./components/MyButton.js";
 
 // vue的根组件
 const template = `
-<div>
-<h1>App组件</h1>
-<MyButton></MyButton>
-</div>
+  <div>
+    <h1>App组件</h1>
+    <MyButton></MyButton>
+  </div>
 `;
  export default {
      components: {MyButton},
