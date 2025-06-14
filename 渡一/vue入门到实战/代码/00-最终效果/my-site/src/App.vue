8bfbd8770109b0cd4f64379c7d813f26c365c839
tian <1163586133@qq.com>
2025-06-14 10:46:00 +0800

    【渡一】 1. 搭建工程-头像组件

---
+++ 渡一/vue入门到实战/代码/00-最终效果/my-site/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <avatar :url="url" />
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+
+</style>
+<script>
+import Avatar from "@/components/Avatar.vue";
+export default {
+  components: {Avatar},
+  data() {
+    return {
+      url: 'https://gips2.baidu.com/it/u=3133340021,3370688897&fm=3042&app=3042&f=JPEG&wm=1,baiduai,0,0,13,9&wmo=0,0&w=640&h=480'
+    }
+  }
+}
+</script>
---
+++ 渡一/vue入门到实战/代码/00-最终效果/my-site/src/components/Avatar.vue
@@ -0,0 +1,31 @@
+<template>
+  <img class="avatar-container" :src="url" :style="{width: size + 'px', height: size + 'px'}" :alt="alt"/>
+</template>
+
+<script>
+export default {
+  name: "Avatar",
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: Number,
+      default: 150
+    },
+    alt: {
+      type: String,
+      default: '',
+    }
+  }
+}
+</script>
+
+<style scoped>
+.avatar-container {
+  border-radius: 50%;
+  object-fit: cover;
+  display: block; /* 这里为什么必须要加一个block？ 因为加到块盒里会导致排编*/
+}
+</style>
