dfc623bcc61df88b545634681fab4f4c9015e139
tian <1163586133@qq.com>
2025-06-14 12:35:13 +0800

    【渡一】 1. 计算属性-完成

--- 渡一/vue入门到实战/代码/00-最终效果/my-site/src/App.vue
+++ 渡一/vue入门到实战/代码/00-最终效果/my-site/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <avatar :url="url" />
+    <Icon type="home" />
     <router-view/>
   </div>
 </template>
@@ -10,8 +11,9 @@
 </style>
 <script>
 import Avatar from "@/components/Avatar.vue";
+import Icon from "@/components/Icon.vue";
 export default {
-  components: {Avatar},
+  components: {Icon, Avatar},
   data() {
     return {
       url: 'https://gips2.baidu.com/it/u=3133340021,3370688897&fm=3042&app=3042&f=JPEG&wm=1,baiduai,0,0,13,9&wmo=0,0&w=640&h=480'
@@ -19,3 +21,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.icon-container {
+  color: red;
+}
+</style>
---
+++ 渡一/vue入门到实战/代码/00-最终效果/my-site/src/components/Icon.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="icon-container">
+    <i class="iconfont" :class="iconClass"></i>
+  </div>
+</template>
+
+<script>
+import {iconClassMap} from "@/modals/iconData";
+
+export default {
+  name: "Icon",
+  props: {
+    type: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    iconClass() {
+      return iconClassMap[this.type];
+    }
+  }
+}
+</script>
+
+<style scoped>
+@import "//at.alicdn.com/t/font_2164449_nalfgtq7il.css";
+</style>
